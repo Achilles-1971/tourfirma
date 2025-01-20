30d1c990bfe63bac797f85dd0f519a5d65b37c20
Achilles, son of Peleus <saikinairiofficial@gmail.com>
2025-01-20 22:30:29 +0300

    v2.1

--- mysite/tours/admin.py
+++ mysite/tours/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Tour, Order, News
 
+
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at')  # Отображение заголовка и даты создания
@@ -13,7 +14,7 @@ class TourAdmin(admin.ModelAdmin):
     list_filter = ('start_date', 'end_date')  # Фильтрация по дате начала и окончания
     search_fields = ('name', 'description')  # Поиск по имени и описанию
     readonly_fields = ('image_preview',)  # Поле предпросмотра только для чтения
-    fields = ('name', 'description', 'price', 'start_date', 'end_date', 'latitude', 'longitude', 'image', 'image_preview')  # Поля формы в админке
+    fields = ('name', 'description', 'price', 'start_date', 'end_date', 'latitude', 'longitude', 'image', 'gif', 'image_preview')  # Поля формы в админке
 
     def image_preview(self, obj):
         """Предпросмотр изображения для тура."""
